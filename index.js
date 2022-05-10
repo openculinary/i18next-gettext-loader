b3b1726594809ec62d029db07564d35793a1dde2
James Addison <james@reciperadar.com>
2022-05-10 18:29:18 +0100

    Remove loader-utils dependency

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const converter = require('i18next-conv')
-const loaderUtils = require('loader-utils')
 
 module.exports = function (source) {
   this.cacheable && this.cacheable()
   const callback = this.async()
-  const options = loaderUtils.getOptions(this) || {}
+  const options = this.getOptions() || {}
 
   const re = new RegExp('Language: (\\w+)')
   const match = source.match(re)
