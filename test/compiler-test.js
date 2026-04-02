858bcb92f16474451bc46e2aacb51110b3914ed2
James Addison <james@reciperadar.com>
2026-04-02 15:59:19 +0100

    Tests: migrate from `jest` to `mocha`
    This reduces our package dependency set fairly significantly, and
    simplifies the repository contents too.

---
+++ test/compiler-test.js
@@ -0,0 +1,12 @@
+import assert from "node:assert";
+
+import compiler from "./fixtures/compiler.js";
+
+describe("compilation", () => {
+  it("generates i18njs from po files", async () => {
+    const stats = await compiler("test.po");
+    const output = stats.toJson({ source: true }).modules[0].source;
+
+    assert.equal(output, `module.exports = {"Hello world!":"Hello world!"}`);
+  });
+});
